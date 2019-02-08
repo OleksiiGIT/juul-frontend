d46071e832eb68dd17e083cc0e5e205a67a66a2f
OleksiiGIT <matalexnin@gmail.com>
2019-02-08 18:46:49 +0200

    second push

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,39 @@
 $(function() {
 
-	// Пользовательские функции
+    $('.fullpageInit').fullpage({
+        autoScrolling:true,
+        scrollHorizontally: true,
+        navigation: true,
+        anchors:['01','02','03','04'],
+        afterLoad: function() {
+			if ($('.whiteBg').hasClass('active')) {
+		    	$('#fp-nav').children('ul').children('li').children('a').css('color', 'black')
+		    }
+        },
+        onLeave : function() {
+			if ($('.whiteBg').hasClass('active')) {
+		    	$('#fp-nav').children('ul').children('li').children('a').css('color', 'white')
+		    }
+        }
+    })
+
+    $.fn.fullpage.setAllowScrolling(true)
+
+    $('.scrollDowmLine').click(e => {
+    	e.preventDefault()
+    	$.fn.fullpage.moveSectionDown()
+    })
+
+    $('.nextBtn').click(e => {
+    	e.preventDefault()
+    	$.fn.fullpage.moveSectionDown()
+    })
+
+    $('.prevBtn').click(e => {
+    	e.preventDefault()
+    	$.fn.fullpage.moveSectionUp()
+    })
+
+    
 
 });
